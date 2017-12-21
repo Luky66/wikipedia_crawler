a2b98b2c930e9962d1e857a50c212f3a3d5ac591
Lukas Bühler <lukasmc6@gmail.com>
2017-12-22 00:26:35 +0100

    some more work

--- crawl.py
+++ crawl.py
@@ -1,1 +1,18 @@
 import requests
+from bs4 import BeautifulSoup
+
+# https://www.instagram.com/luky_66/
+
+def insta_friend_spider(max_photos):
+    url = "https://pixabay.com/en/users/melancholiaphotography-2312503/"
+    html = requests.get(url)
+    plain_text = html.text
+    soup = BeautifulSoup(plain_text, "lxml")
+
+    for item in soup.find_all("div", {"class": "item"}):
+        src = item.a.img.get("src")
+        print(src)
+
+
+print("Hello World")
+insta_friend_spider(5)

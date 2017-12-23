2ca81747948dc1c191aa86d3d424a43e87457a53
Lukas Bühler <lukasmc6@gmail.com>
2017-12-23 23:53:19 +0100

    some more adjustments

--- crawl.py
+++ crawl.py
@@ -9,40 +9,43 @@ def wikipedia_links_to_page(startPage, targetPage):
     done = False
 
     uncrawled_pages.append(crawl_wikipedia_page(startPage))
+    print("Started with "+str(len(uncrawled_pages[0]))+" links")
 
-    while not done and not uncrawled_pages:
+    while not done or not uncrawled_pages:
 
         # add the links of the start page to the list
 
-        for item in uncrawled_pages[0][0]:
+        for item in uncrawled_pages[0]:
             link = item["href"]
             
             if link == targetPage:
-                print("We found the page with link: "+link)
                 done = True
+                break
             
         if not done:
-            uncrawled_pages.append(crawl_wikipedia_page(uncrawled_pages[0][0]))
+            print("Crawling new page: "+str(uncrawled_pages[0][0]))
+            jumps += 1
+
+            uncrawled_pages.append(crawl_wikipedia_page(uncrawled_pages[0][0]["href"]))
             del uncrawled_pages[0][0]
             if not uncrawled_pages[0]:
                 del uncrawled_pages[0]
     
     if done:
-        print("The target page was found")
+        print("The target page '"+targetPage+"' was found with "+str(jumps+1)+" jumps")
     else:
         print("the target page wasn't found")
 
 
 
 def crawl_wikipedia_page(page):
+    print("Crawling "+page)
     html = requests.get(page)
     plain_text = html.text
     soup = BeautifulSoup(plain_text, "html.parser")
     wiki_text = soup.body.find("div", {"id": "mw-content-text"}).div
 
-    return wiki_text.findAll("a")
+    return wiki_text.findAll("a", {"href": True})
 
 
-wikipedia_links_to_page(
-    "https://en.wikipedia.org/wiki/World_War_II",
-    "https://en.wikipedia.org/wiki/Adolf_Hitler")
+wikipedia_links_to_page("https://en.wikipedia.org/wiki/World_War_II", "/wiki/Adolf_Hitler")

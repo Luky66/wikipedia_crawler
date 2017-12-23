60c159ab0df52a09817d317c6a9ee108378224cd
Lukas Bühler <lukasmc6@gmail.com>
2017-12-23 23:16:08 +0100

    changed the whole shebang

--- crawl.py
+++ crawl.py
@@ -1,18 +1,47 @@
 import requests
 from bs4 import BeautifulSoup
 
-# https://www.instagram.com/luky_66/
 
-def insta_friend_spider(max_photos):
-    url = "https://pixabay.com/en/users/melancholiaphotography-2312503/"
-    html = requests.get(url)
+def wikipedia_links_to_page(startPage, targetPage):
+    jumps = 0
+    jump_points = []
+    uncrawled_pages = []
+    done = False
+
+    uncrawled_pages.append(crawl_wikipedia_page(startPage))
+
+    while done != True and len(uncrawled_pages) != 0
+
+        # add the links of the start page to the list
+        
+
+        for item in wikiText.findAll("a"):
+            link = item["href"]
+            
+            if link == targetPage:
+                print("We found the page with link: "+link)
+                done = True
+            
+        if done == False:
+            uncrawled_pages.append(crawl_wikipedia_page(uncrawled_pages[0][0]))
+            del uncrawled_pages[0][0]
+            if len(uncrawled_pages[0]) == 0:
+                del uncrawled_pages[0]
+    
+    if done:
+        print("The target page was found")
+    else:
+        print("the target page wasn't found")
+
+
+
+def crawl_wikipedia_page(page):
+    html = requests.get(startPage)
     plain_text = html.text
-    soup = BeautifulSoup(plain_text, "lxml")
+    soup = BeautifulSoup(plain_text, "html.parser")
+    wikiText = soup.body.find("div", {"id": "mw-content-text"}).div
 
-    for item in soup.find_all("div", {"class": "item"}):
-        src = item.a.img.get("src")
-        print(src)
+    return wikiText.findAll("a")
 
 
-print("Hello World")
-insta_friend_spider(5)
+findWikiPageFromLinks("https://en.wikipedia.org/wiki/World_War_II", "https://en.wikipedia.org/wiki/Adolf_Hitler")
